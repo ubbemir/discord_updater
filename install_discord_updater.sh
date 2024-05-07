6b62447851316ccd6febbcd0d5b95e44e61fce76
ubbemir <61583055+ubbemir@users.noreply.github.com>
2024-05-07 21:34:56 +0200

    Update install_discord_updater.sh

--- install_discord_updater.sh
+++ install_discord_updater.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 update_script_name="update_discord.sh"
+install_dir="$HOME/bin"
 
 install_discord_update_script() {
     echo "Installing Discord update script..."
-    mkdir -p ~/bin
-    cp "$update_script_name" ~/bin/
-    chmod +x ~/bin/"$update_script_name"
+    mkdir -p "$install_dir"
+    cp "$update_script_name" "$install_dir/"
+    chmod +x "$install_dir/$update_script_name"
 }
 
 create_autostart_desktop_entry() {
     echo "Creating autostart desktop entry..."
-    mkdir -p ~/.config/autostart
-    cat <<EOF > ~/.config/autostart/discord_updater.desktop
+    autostart_dir="$HOME/.config/autostart"
+    mkdir -p "$autostart_dir"
+    cat <<EOF > "$autostart_dir/discord_updater.desktop"
 [Desktop Entry]
 Type=Application
-Exec=gnome-terminal -- ~/bin/"$update_script_name"
+Exec=gnome-terminal -- "$install_dir/$update_script_name"
 Hidden=false
 NoDisplay=false
 X-GNOME-Autostart-enabled=true
 Name=Discord Updater
 EOF
 }
-
 
 main() {
     install_discord_update_script
